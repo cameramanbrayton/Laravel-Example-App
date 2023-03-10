870eb44e3f7ff2db4046f41146ac6540840b46d8
cameramanbrayton <braytonscottproductions@gmail.com>
2023-03-09 20:57:33 -0600

    Laravel Example App

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\ListingController;
-use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -42,6 +43,8 @@ Route::put('/listings/{listing}', [ListingController::class, 'update']);
 // Delete Listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']); 
 
-
 // Single Listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+// Show Register/Create Form
+Route::get('/register', [UserController::class, 'create']);
